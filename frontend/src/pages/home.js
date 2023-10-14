f7fdbc8235510700a6e7ddac4ffaa75cbb0cb71c
Mani kandan <manicdon7@example.com>
2023-10-14 20:35:58 +0530

    nav

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,9 +1,14 @@
+import Navbar from "../components/navbar";
+
 const Home = () => {
     return (
         <div className="h-screen">
+            <Navbar />
+            <div className="flex justify-center">
             <h1 className="text-2xl font-bold font-serif pl-5 text-white mt-16 ">
                 Designing Beyond Today, for Tomorrow
             </h1>
+            </div>
         </div>
     );
 }
