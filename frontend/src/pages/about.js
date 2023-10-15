b6da687630a27ca58a7717fe63d475404096febc
Sunil_0881 <sunil12162003@gmail.com>
2023-10-15 16:17:50 +0530

    fc

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar";
 import Slider from "../components/Slideabout";
 import Team from "../components/Team";
+import Footer from "../components/Footer"
 
 const About = () => {
     return ( 
