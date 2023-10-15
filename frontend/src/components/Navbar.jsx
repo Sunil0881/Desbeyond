2f10372557fa4a1d246600cfc171f95007cf8b5e
Deepakraja03 <12306deepakraja@gmail.com>
2023-10-15 18:13:24 +0530

    navbar updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import logo from '../assets/Desbeyondlogo.png'
+import logo from '../assets/logo.png'
 
 
 
@@ -11,7 +11,7 @@ function NavLink({to, children}) {
 
 function MobileNav({open, setOpen}) {
     return (
-        <div className={`absolute top-0 left-0 h-screen w-screen bg-gradient-to-b from-purple-600 via-gray-200 to-blue-400 z-10 transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
+        <div className={`absolute top-0 left-0 h-screen w-screen bg-gradient-to-r from-violet-600 via-purple-800 to-violet-800 z-10 transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
             <div className="flex items-center justify-center filter drop-shadow-md font-bold g-gradient-to-r from-purple-600 via-gray-200 to-blue-400 h-20 z-10 md:hidden"> {/*logo container*/}
                 <h1 className="text-xl font-poppins text-black uppercase font-bold">Desbeyond</h1>
             </div>
@@ -43,12 +43,18 @@ export default function Navbar() {
 
     const [open, setOpen] = useState(false)
     return (
-        <nav className="flex filter drop-shadow-md bg-gradient-to-r from-purple-600 via-gray-200 to-blue-400 bg-transparent px-4 py-4 h-20 items-center">
+        <nav className="flex filter drop-shadow-md bg-gradient-to-r from-violet-600 via-purple-800 to-violet-800 bg-transparent px-4 py-4 h-20 items-center">
             <MobileNav open={open} setOpen={setOpen}/>
-            <div className="w-3/12 flex items-center">
-                <a className="flex text-2xl font-semibold" href="/"><img src={logo} className='h-10 md:h-20 md:m-5 w-full' alt='logo' /></a>
+            <div className="w-2/12 flex items-center">
+                <a className="flex" href="/"><img src={logo} className='h-12 w-full' alt='logo' /></a>
+                <div className='pt-2'>
+                    <a href='/'>
+                    <h1 className='text-white font-bold text-2xl uppercase'>desbeyond</h1>
+                    <h1 className='text-white font-semibold text-xs tracking-widest'>Design Beyond</h1>
+                    </a>
+                </div>
             </div>
-            <div className="w-9/12 flex justify-end items-center text-black">
+            <div className="w-10/12 flex justify-end items-center text-black">
                 <div className="z-50 flex relative w-8 h-8 flex-col justify-between items-center md:hidden" onClick={() => {
                     setOpen(!open)
                 }}>
@@ -57,21 +63,21 @@ export default function Navbar() {
                     <span className={`h-1 w-full bg-black rounded-lg transition-all duration-300 ease-in-out md:hidden ${open ? "w-0" : "w-full"}`} />
                     <span className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out md:hidden ${open ? "-rotate-45 -translate-y-3.5" : ""}`} />
                 </div>
-                <div className="hidden font-semibold md:flex">
+                <div className="hidden text-white font-semibold md:flex">
                     <NavLink to="/">
-                        HOME
+                        <h1 className=" hover:text-cyan-500">HOME</h1>
                     </NavLink>
                     <NavLink to="/contact">
-                        CONTACT
+                        <h1 className='hover:text-cyan-500'>CONTACT</h1>
                     </NavLink>
                     <NavLink to="/about">
-                        ABOUT
+                        <h1 className='hover:text-cyan-500'>ABOUT</h1>
                     </NavLink>
                     <NavLink to="/Service">
-                        SERVICES
+                        <h1 className='hover:text-cyan-500'>SERVICES</h1>
                     </NavLink>
                     <NavLink to="/blogs">
-                        BLOGS
+                        <h1 className='hover:text-cyan-500'>BLOGS</h1>
                     </NavLink>
                 </div>
             </div>
